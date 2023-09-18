39ea42a2cac91bc8370b65545d566b31b0e69bcf
pintariching <tilen.pintaric99@gmail.com>
2023-09-18 23:06:40 +0200

    Ditch self written bootloader

--- build.rs
+++ build.rs
@@ -1,93 +1,25 @@
-use std::path::{Path, PathBuf};
-use std::process::Command;
-
-const BOOTLOADER_VERSION: &str = env!("CARGO_PKG_VERSION");
+use std::path::PathBuf;
 
 fn main() {
-    let out_dir = PathBuf::from(std::env::var("OUT_DIR").unwrap());
+    // set by cargo, build scripts should use this directory for output files
+    let out_dir = PathBuf::from(std::env::var_os("OUT_DIR").unwrap());
+    // set by cargo's artifact dependency feature, see
+    // https://doc.rust-lang.org/nightly/cargo/reference/unstable.html#artifact-dependencies
+    let kernel = PathBuf::from(std::env::var_os("CARGO_BIN_FILE_KERNEL_kernel").unwrap());
 
-    bios_main(&out_dir);
+    // create an UEFI disk image (optional)
+    let uefi_path = out_dir.join("uefi.img");
+    bootloader::UefiBoot::new(&kernel)
+        .create_disk_image(&uefi_path)
+        .unwrap();
 
-    let bios_path = out_dir.join("bin").join("bootloader-stage-1.bin");
+    // create a BIOS disk image
+    let bios_path = out_dir.join("bios.img");
+    bootloader::BiosBoot::new(&kernel)
+        .create_disk_image(&bios_path)
+        .unwrap();
+
+    // pass the disk image paths as env variables to the `main.rs`
+    println!("cargo:rustc-env=UEFI_PATH={}", uefi_path.display());
     println!("cargo:rustc-env=BIOS_PATH={}", bios_path.display());
-}
-
-fn bios_main(out_dir: &Path) {
-    let boot_sector_path = build_bios_boot_sector(&out_dir);
-
-    println!(
-        "cargo:rustc-env=BIOS_BOOT_SECTOR_PATH={}",
-        boot_sector_path.display()
-    );
-}
-
-fn build_bios_boot_sector(out_dir: &Path) -> PathBuf {
-    let cargo = std::env::var("CARGO").unwrap_or_else(|_| "cargo".into());
-
-    let mut cmd = Command::new(cargo);
-    cmd.arg("install").arg("bootloader-stage-1");
-
-    let local_path = Path::new(env!("CARGO_MANIFEST_DIR"))
-        .join("rustos-bootloader")
-        .join("stage-1");
-
-    if local_path.exists() {
-        cmd.arg("--path").arg(&local_path);
-        println!("cargo:rerun-if-changed={}", local_path.display());
-    } else {
-        cmd.arg("--version").arg(BOOTLOADER_VERSION);
-    }
-
-    cmd.arg("--locked");
-    cmd.arg("--target").arg("i386-code16-boot-sector.json");
-    cmd.arg("--profile").arg("stage-1");
-    cmd.arg("-Zbuild-std=core")
-        .arg("-Zbuild-std-features=compiler-builtins-mem");
-    cmd.arg("--root").arg(out_dir);
-    cmd.env_remove("RUSTFLAGS");
-    cmd.env_remove("CARGO_ENCODED_RUSTFLAGS");
-    cmd.env_remove("RUSTC_WORKSPACE_WRAPPER");
-
-    let status = cmd
-        .status()
-        .expect("failed to run cargo install for bios bootsector");
-    let elf_path = if status.success() {
-        let path = out_dir.join("bin").join("bootloader-stage-1");
-        assert!(
-            path.exists(),
-            "bios boot sector executable does not exist after building"
-        );
-        path
-    } else {
-        panic!("failed to build bios boot sector");
-    };
-
-    convert_elf_to_bin(elf_path)
-}
-
-fn convert_elf_to_bin(elf_path: PathBuf) -> PathBuf {
-    let flat_binary_path = elf_path.with_extension("bin");
-
-    let llvm_tools = llvm_tools::LlvmTools::new().expect("failed to get llvm tools");
-    let objcopy = llvm_tools
-        .tool(&llvm_tools::exe("llvm-objcopy"))
-        .expect("LlvmObjcopyNotFound");
-
-    // convert first stage to binary
-    let mut cmd = Command::new(objcopy);
-    cmd.arg("-I").arg("elf64-x86-64");
-    cmd.arg("-O").arg("binary");
-    cmd.arg("--binary-architecture=i386:x86-64");
-    cmd.arg(&elf_path);
-    cmd.arg(&flat_binary_path);
-    let output = cmd
-        .output()
-        .expect("failed to execute llvm-objcopy command");
-    if !output.status.success() {
-        panic!(
-            "objcopy failed: {}",
-            String::from_utf8_lossy(&output.stderr)
-        );
-    }
-    flat_binary_path
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,20 @@
-mod disk;
-
 fn main() {
+    // read env variables that were set in build script
+    let uefi_path = env!("UEFI_PATH");
     let bios_path = env!("BIOS_PATH");
-    let mut cmd = std::process::Command::new("qemu-system-x86_64");
-    cmd.arg("-drive")
-        .arg(format!("format=raw,file={bios_path}"));
 
+    // choose whether to start the UEFI or BIOS image
+    let uefi = true;
+
+    let mut cmd = std::process::Command::new("qemu-system-x86_64");
+    if uefi {
+        cmd.arg("-bios").arg(ovmf_prebuilt::ovmf_pure_efi());
+        cmd.arg("-drive")
+            .arg(format!("format=raw,file={uefi_path}"));
+    } else {
+        cmd.arg("-drive")
+            .arg(format!("format=raw,file={bios_path}"));
+    }
     let mut child = cmd.spawn().unwrap();
     child.wait().unwrap();
 }
